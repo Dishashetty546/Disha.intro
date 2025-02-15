5811135c65af4cdbd56a79df131ef3070fd3c480
dishashetty546 <dishashetty546@gmail.com>
2025-02-15 17:18:56 +0530

    UPDATED

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -6,7 +6,7 @@ const Contacts = () => {
       <h1 className='my-10 text-center text-4xl'>Get In Touch</h1>
       <div className='text-center tracking-tighter'>
         <p className='my-4'>{CONTACT.address}</p>
-        <p className='my-4'>{CONTACT.phoneNo}</p>
+        
         <a href="#" className='border-b'>{CONTACT.email}</a>
       </div>
     </div>
